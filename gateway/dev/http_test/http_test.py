ba4c389bfa8ab64df96aed37392f9da64465aba2
BojanMilosevic-FBK <milosevic@fbk.eu>
2017-11-29 18:06:20 +0100

    BM: serial development

--- gateway/dev/http_test/http_test.py
+++ gateway/dev/http_test/http_test.py
@@ -16,33 +16,96 @@
 # }
 # ]
 
+# contact data array formed by:
+# {
+#     "wsnNodeId" : <BeaconID> "string"
+#     "eventType" : 901,
+#     "timestamp" : <timestamp>,
+#     "payload" : {
+# 		"EndNodeID"   : "string"
+# 		"lastRSSI"   : <int>
+# 		"maxRSSI"  	 : <int>
+# 		"pktCounter" : <int>
+#     }
+# }
+
+
 
 import requests
 import json
-import datetime
+import time
 
+EVENT_BECON_CONTACT = 901;
 
 print("Creating url and header...")
 
-url = 'https://climbdev.smartcommunitylab.it/v2/api/event/TEST/adca3db3-68d1-4197-b834-a45d61cf1c21/vlab'
+urlDev_CLIMB = 'https://climbdev.smartcommunitylab.it/v2/api/event/TEST/adca3db3-68d1-4197-b834-a45d61cf1c21/vlab'
+
+urlDev = 'https://climbdev.smartcommunitylab.it/v2/api/event/TEST/4220a8bb-3cf5-4076-b7bd-9e7a1ff7a588/vlab'
+urlProd = ' https://climb.smartcommunitylab.it/v2/api/event/TEST/17ee8383-4cb0-4f58-9759-1d76a77f9eff/vlab'
 # headers = {'Authorization': 'Bearer 831a2cc0-48bd-46ab-ace1-c24f767af8af', 'Content-Type': 'application/json'}
+
 headers = {'Authorization': 'Bearer 831a2cc0-48bd-46ab-ace1-c24f767af8af'}
 
 # timeNow = datetime.datetime.now()
 # print("Timestamp:", timeNow)
 
+timestamp = int(time.time())
+print("Timestamp :", timestamp)
 
-payloadData = [{
-    "wsnNodeId" : "Node01",
+lastRSSI = -30
+maxRSSI = -20
+
+
+payloadDataS = [
+{
+    "wsnNodeId" : "Beaconid_01",
     "eventType" : 901,
-    "timestamp" : 1511361257,
+    "timestamp" : timestamp,
     "payload" : {
-        "passengerId" : "a511a089-ab7a-446f-a8c2-4c208d4425c5",
-        "latitude" : 46.0678106,
-        "longitude" : 11.1515548,
-        "accuracy" : 18.5380001068115
+		"EndNodeID": "VelaLab_EndNode_05",
+		"lastRSSI": lastRSSI,
+		"maxRSSI": maxRSSI,
+		"pktCounter" : 30
     }
-}]
+}
+]
+
+payloadData = [
+{
+    "wsnNodeId" : "Beaconid_01",
+    "eventType" : EVENT_BECON_CONTACT,
+    "timestamp" : timestamp,
+    "payload" : {
+		"EndNodeID": "VelaLab_EndNode_05",
+		"lastRSSI": lastRSSI,
+		"maxRSSI": maxRSSI,
+		"pktCounter" : 30
+    }
+},
+{
+    "wsnNodeId" : "Beaconid_01",
+    "eventType" : EVENT_BECON_CONTACT,
+    "timestamp" : timestamp,
+    "payload" : {
+		"EndNodeID" : "VelaLab_EndNode_05",
+		"lastRSSI" : lastRSSI,
+		"maxRSSI" : maxRSSI,
+		"pktCounter" : 20
+    }
+},
+{
+    "wsnNodeId" : "Beaconid_01",
+    "eventType" : EVENT_BECON_CONTACT,
+    "timestamp" : timestamp,
+    "payload" : {
+		"EndNodeID" : "VelaLab_EndNode_03",
+		"lastRSSI" : lastRSSI,
+		"maxRSSI" : maxRSSI,
+		"pktCounter" : 14
+    }
+}
+]
 
 
 print("\nData:")
@@ -53,7 +116,7 @@ jsonData = json.dumps(payloadData)
 print("\njsonData:")
 print(jsonData)
 
-r = requests.post(url, json=payloadData, headers=headers)
+r = requests.post(urlDev_CLIMB, json=payloadDataS, headers=headers)
 print("\nResponse:")
 print(r.text)
 
--- gateway/dev/serial_test/serial_test_printchar.py
+++ gateway/dev/serial_test/serial_test_printchar.py
@@ -1,41 +1,103 @@
 
 import serial
 from array import array
+import logging
+import datetime
+import time
 
+# constantd and defines
+# start character = 42 (0x2A)
+# start sequence = 4 times start char = 42, 42, 42, 42
+START_BUF = 0x2A2A2A
+# print("startChar:", START_BUF, "type:", type(START_BUF))
+
+LOG_LEVEL = logging.DEBUG
+
+
+timestr = time.strftime("%Y%m%d-%H%M%S")
+# print(timestr)
+
+filenameLog = timestr + "-application.log"
+print(filenameLog)
+
+# Log init
+logging.basicConfig(filename=filenameLog,level=LOG_LEVEL,format='%(message)s')
+# logging.debug("[" + time.strftime("%H%M%S") + ']' + ' This message should go to the log file')
+logging.debug('# Time \tNodeID \tLast \tCounter \tDataLen')
+
+
+# Serial init
 ser = serial.Serial()
 ser.port = "/dev/ttyACM0"
-ser.baudrate = 1000000
+#ser.baudrate = 1000000
+ser.baudrate = 115200
 
 print('Opening port:', ser.port)
 ser.open()
-ser.read(ser.inWaiting())   # read and discard input buffer
+#ser.read(ser.inWaiting())   # read and discard input buffer
+ser.reset_input_buffer()
 
 while(1):
     if ser.isOpen():
 
-        if ser.inWaiting()>0:
+        bytesWaiting = ser.inWaiting()
+        if bytesWaiting > 0:
+
+            # print("Serial Waiting:", bytesWaiting)
+            # bufferSerial = ser.read(bytesWaiting)
+            # print(bufferSerial)
+            # startChar = 1;
 
             startChar = int.from_bytes(ser.read(1), byteorder='big', signed=False)
-            #print("startChar:", startChar, "type:", type(startChar))
+            # print("startChar:", startChar, "type:", type(startChar))
 
             if startChar == 42:
-                # counter = ser.read(1)
-                counter = int.from_bytes(ser.read(1), byteorder='little', signed=False)
-                # print("Counter:", counter, "type:", type(counter))
 
-                pktLast = (counter & 128)
-                pktCount = counter & 127
+                # print("received 1 Start char. inWaiting:", bytesWaiting)
+                startBuf = int.from_bytes(ser.read(3), byteorder='little', signed=False)
+                # print("startBuf:", startBuf, "type:", type(startBuf))
 
-                tmpBuf = ser.read(2)
-                dataLen = int.from_bytes(tmpBuf, byteorder='little', signed=False)
-                # print("Data Length:", dataLen, "type:", type(dataLen))
+                if startBuf == START_BUF:
 
-                dataBuf = ser.read(dataLen)
-                lastChar = ser.read(1)
+                    # print("Received START")
 
-                print("PKT read! last:", pktLast, " counter:", pktCount, "dataLen:", dataLen)
-                # print("Data type:", type(dataBuf))
-                # print(dataBuf)
+                    nodeID = int.from_bytes(ser.read(1), byteorder='little', signed=False)
+                    # print("nodeID:", nodeID, "type:", type(nodeID))
+
+                    # counter = ser.read(1)
+                    counter = int.from_bytes(ser.read(1), byteorder='little', signed=False)
+                    pktLast = (counter & 128)
+                    pktCount = counter & 127
+                    # print("Counter:", pktCount, "type:", type(pktCount))
+
+                    tmpBuf = ser.read(2)
+                    dataLen = int.from_bytes(tmpBuf, byteorder='little', signed=False)
+                    # print("Data Length:", dataLen, "type:", type(dataLen))
+
+                    if dataLen < 500:
+
+                        dataBuf = ser.read(dataLen)
+
+                        # print("PKT read! node:", nodeID, " last:", pktLast, " counter:", pktCount, "dataLen:", dataLen, "last bytes:", dataBuf[dataLen-8], dataBuf[dataLen-7], dataBuf[dataLen-6], dataBuf[dataLen-5], dataBuf[dataLen-4], dataBuf[dataLen-3], dataBuf[dataLen-2], dataBuf[dataLen-1])
+                        print("PKT read! node:", nodeID, "\tlast:", pktLast, "\tcounter:", pktCount, "\tdataLen:", dataLen, "\tlast bytes:", dataBuf[dataLen-16:])
+                        # logging.debug("%s; %s; %s; %s; %s;", time.strftime("%H%M%S"), nodeID, pktLast, pktCount, dataLen)
+                        logging.debug("%s\t%s\t%s\t%s\t%s", time.strftime("%H%M%S"), nodeID, pktLast, pktCount, dataLen)
+
+                        lastChar = dataBuf[dataLen-1]
+                        if lastChar != 10:
+                            ser.reset_input_buffer()
+
+
+                        # print("\n")
+                        # print("Data type:", type(dataBuf))
+                        # print(dataBuf)
+                        # print("\n")
+
+                    else:
+                        bufferSerial = ser.read(ser.inWaiting())
+                        print("\nBuffer Serial:")
+                        print(bufferSerial)
+
 
             # print("#bytes to read:", nBytesIn)
             # print(bufferString)
