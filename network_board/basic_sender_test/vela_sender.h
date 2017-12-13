8dcb0e0b524fe6b12af9a0e09a470e72b90ef1e8
Davide Giovanelli <dgiovanelli@fbk.eu>
2017-12-13 17:57:40 +0100

    Code clean

--- network_board/basic_sender_test/vela_sender.h
+++ network_board/basic_sender_test/vela_sender.h
@@ -3,12 +3,12 @@
 
 PROCESS_NAME (vela_sender_process);
 
-#define MAX_NUMBER_OF_BT_BEACONS 100
-#define SINGLE_NODE_REPORT_SIZE 12
-#define MAX_MESH_PAYLOAD_SIZE	MAX_NUMBER_OF_BT_BEACONS*SINGLE_NODE_REPORT_SIZE
+//#define MAX_NUMBER_OF_BT_BEACONS 100	//defined in constraints.h
+//#define SINGLE_NODE_REPORT_SIZE 12	//defined in uart_util.h
+//#define MAX_MESH_PAYLOAD_SIZE	MAX_NUMBER_OF_BT_BEACONS*SINGLE_NODE_REPORT_SIZE
 
 
-#define MAX_RPL_SEND_SIZE 90  // needs to be a multiple of 9
+#define MAX_RPL_SEND_SIZE 90  // needs to be a multiple of SINGLE_NODE_REPORT_SIZE
 
 
 // the event to be raised between the uart and the sender
